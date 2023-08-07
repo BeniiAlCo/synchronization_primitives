dfcc3c74b69699a0076048661d95f64d46a4a9f0
Benii <benii.alco@gmail.com>
2023-08-07 20:09:04 +0100

    queue with atomic push and pop that now shouldn't miss any calls to push

--- src/queue_based_lock.rs
+++ src/queue_based_lock.rs
@@ -24,10 +24,9 @@ impl<T> Queue<T> {
 
     pub fn push(&self, elem: T) {
         let new_tail = Box::into_raw(Box::new(Node { elem, next: None }));
-
+        let mut current_tail = self.tail.load(Relaxed);
         loop {
             unsafe {
-                let mut current_tail = self.tail.load(Relaxed);
                 if current_tail.is_null() {
                     // Tail has nothing -- we need to add a new tail, and make the head point to that
                     // tail as it is the only element in the queue.
@@ -54,14 +53,17 @@ impl<T> Queue<T> {
                     }
                 } else {
                     // Tail has something, so we need to update the old Tail
-                    let old_tail = current_tail;
-                    (*old_tail).next = Some(NonNull::new_unchecked(new_tail));
+                    //let old_tail = current_tail;
 
                     match self
                         .tail
-                        .compare_exchange(old_tail, new_tail, Acquire, Relaxed)
+                        .compare_exchange(current_tail, new_tail, Acquire, Relaxed)
                     {
-                        Ok(_) => break,
+                        Ok(ptr) => {
+                            (*ptr).next = Some(NonNull::new_unchecked(new_tail));
+                            //self.tail.store(new_tail, Relaxed);
+                            break;
+                        }
                         Err(e) => {
                             current_tail = e;
                         }
@@ -73,10 +75,11 @@ impl<T> Queue<T> {
 
     pub fn pop(&self) -> Option<T> {
         let current_head = self.head.load(Relaxed);
+        let current_tail = self.tail.load(Relaxed);
         unsafe {
-            if current_head.is_null() {
+            if current_head.is_null() && current_tail.is_null() {
                 None
-            } else {
+            } else if !current_head.is_null() && !current_tail.is_null() {
                 match self
                     .tail
                     .compare_exchange(current_head, null_mut(), Acquire, Relaxed)
@@ -93,6 +96,8 @@ impl<T> Queue<T> {
                         Some(Box::from_raw(current_head).elem)
                     }
                 }
+            } else {
+                None
             }
         }
     }
@@ -106,105 +111,123 @@ impl<T> Drop for Queue<T> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::{sync::Arc, thread};
+    use super::Queue;
 
-    #[test]
-    fn basics() {
-        let mut list = Queue::new();
+    #[cfg(not(loom))]
+    mod sequential {
+        use super::Queue;
 
-        // Check empty list behaves right
-        assert_eq!(list.pop(), None);
+        #[test]
+        fn basics() {
+            let mut list = Queue::new();
 
-        // Populate list
-        list.push(1);
-        list.push(2);
-        list.push(3);
+            // Check empty list behaves right
+            assert_eq!(list.pop(), None);
 
-        // Check normal removal
-        assert_eq!(list.pop(), Some(1));
-        assert_eq!(list.pop(), Some(2));
+            // Populate list
+            list.push(1);
+            list.push(2);
+            list.push(3);
 
-        // Push some more just to make sure nothing's corrupted
-        list.push(4);
-        list.push(5);
+            // Check normal removal
+            assert_eq!(list.pop(), Some(1));
+            assert_eq!(list.pop(), Some(2));
 
-        // Check normal removal
-        assert_eq!(list.pop(), Some(3));
-        assert_eq!(list.pop(), Some(4));
+            // Push some more just to make sure nothing's corrupted
+            list.push(4);
+            list.push(5);
 
-        // Check exhaustion
-        assert_eq!(list.pop(), Some(5));
-        assert_eq!(list.pop(), None);
+            // Check normal removal
+            assert_eq!(list.pop(), Some(3));
+            assert_eq!(list.pop(), Some(4));
 
-        // Check the exhaustion case fixed the pointer right
-        list.push(6);
-        list.push(7);
+            // Check exhaustion
+            assert_eq!(list.pop(), Some(5));
+            assert_eq!(list.pop(), None);
 
-        // Check normal removal
-        assert_eq!(list.pop(), Some(6));
-        assert_eq!(list.pop(), Some(7));
-        assert_eq!(list.pop(), None);
+            // Check the exhaustion case fixed the pointer right
+            list.push(6);
+            list.push(7);
 
-        // Check dropping the queue
-        list.push(8);
-        list.push(9);
-        list.push(10);
-        drop(list);
+            // Check normal removal
+            assert_eq!(list.pop(), Some(6));
+            assert_eq!(list.pop(), Some(7));
+            assert_eq!(list.pop(), None);
+
+            // Check dropping the queue
+            list.push(8);
+            list.push(9);
+            list.push(10);
+            drop(list);
+        }
     }
 
-    #[test]
-    fn push_from_multiple_threads() {
-        loom::model(|| {
-            const NUM_THREADS: usize = 3;
+    //#[cfg(loom)]
+    mod loom {
+        use super::Queue;
+        use crate::{sync::Arc, thread};
 
-            let list = Arc::new(Queue::new());
+        #[test]
+        fn push_from_multiple_threads() {
+            loom::model(|| {
+                const NUM_THREADS: usize = 3;
 
-            let mut handles = Vec::with_capacity(NUM_THREADS);
+                let list = Arc::new(Queue::new());
 
-            for i in 0..NUM_THREADS {
-                let list = list.clone();
+                let mut handles = Vec::with_capacity(NUM_THREADS);
 
+                for i in 0..NUM_THREADS {
+                    let list = list.clone();
+
+                    let handle = thread::spawn(move || {
+                        list.push(thread::current().id());
+                    });
+                    handles.push(handle);
+                }
+
+                for handle in handles {
+                    handle.join().unwrap();
+                }
+
+                for _ in 0..NUM_THREADS {
+                    assert!(list.pop().is_some());
+                    //dbg!(list.pop());
+                }
+            });
+        }
+
+        #[test]
+        fn push_and_pop_from_multiple_threads() {
+            loom::model(|| {
+                const NUM_THREADS: usize = 3;
+
+                let list = Arc::new(Queue::new());
+
+                let mut handles = Vec::with_capacity(NUM_THREADS);
+
+                let l = list.clone();
                 let handle = thread::spawn(move || {
-                    list.push(thread::current().id());
-                    //assert_ne!(0, list.len());
+                    l.push(thread::current().id());
                 });
                 handles.push(handle);
-            }
 
-            for handle in handles {
-                handle.join().unwrap();
-            }
+                let l = list.clone();
+                let handle = thread::spawn(move || {
+                    l.push(thread::current().id());
+                });
+                handles.push(handle);
 
-            //assert_eq!(NUM_THREADS, list.len());
-        });
-    }
+                let l = list;
+                let handle = thread::spawn(move || {
+                    l.pop();
+                });
+                handles.push(handle);
 
-    #[test]
-    fn push_and_pop_from_multiple_threads() {
-        loom::model(|| {
-            const NUM_THREADS: usize = 3;
-
-            let list = Arc::new(Queue::new());
-
-            let mut handles = Vec::with_capacity(NUM_THREADS);
-
-            let l = list.clone();
-            let handle = thread::spawn(move || {
-                l.push(thread::current().id());
+                for handle in handles {
+                    handle.join().unwrap();
+                }
             });
-            handles.push(handle);
-
-            let l = list;
-            let handle = thread::spawn(move || {
-                l.pop();
-            });
-            handles.push(handle);
-
-            for handle in handles {
-                handle.join().unwrap();
-            }
-        });
+        }
     }
 }
 
